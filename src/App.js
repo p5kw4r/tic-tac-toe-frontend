a9f0b2bdbebe308eefcb0b038f3f13a417f99b68
Patrick Widmer <padi.widmer@gmail.com>
2018-03-25 16:36:41 +0200

    Improve logs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'));
 const abi = [{"constant":true,"inputs":[],"name":"BET_SIZE","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"gameId","type":"uint256"}],"name":"GameCreated","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"player","type":"address"}],"name":"PlayerJoined","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"player","type":"address"}],"name":"NextPlayer","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"winner","type":"address"}],"name":"GameOverWithWin","type":"event"},{"anonymous":false,"inputs":[],"name":"GameOverWithDraw","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"recipient","type":"address"},{"indexed":false,"name":"amountInWei","type":"uint256"}],"name":"PayoutSuccess","type":"event"},{"constant":false,"inputs":[],"name":"createGame","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"gameId","type":"uint256"}],"name":"joinGame","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"gameId","type":"uint256"}],"name":"getBoard","outputs":[{"name":"","type":"address[3][3]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"gameId","type":"uint256"},{"name":"row","type":"uint8"},{"name":"column","type":"uint8"}],"name":"placeMark","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}];
-const address = '0xc0c316e29d7567658b344300cc89f858c9c87868';
+const address = '0x7d1bf7e89c8e1972bd5416b1005e801ed10b21b4';
 
 const contract = new web3.eth.Contract(abi, address);
 const { createGame, joinGame, getBoard, placeMark, BET_SIZE } = contract.methods;
@@ -38,10 +38,10 @@ class App extends Component {
           this.handleNextPlayer(event);
           break;
         case 'GameOverWithWin':
-          this.handleGameOver(`Game is over, winner is ${event.returnValues.winner}.`);
+          this.handleGameOver(`Game is over, winner is ${event.returnValues.winner}`);
           break;
         case 'GameOverWithDraw':
-          this.handleGameOver('Game is over, ended with draw.');
+          this.handleGameOver('Game is over, ended with draw');
           break;
         case 'PayoutSuccess':
           this.handlePayoutSuccess(event);
@@ -53,14 +53,14 @@ class App extends Component {
     BET_SIZE()
       .call()
       .then((betSize) => {
-        console.log(`Bet size is ${betSize} wei.`);
+        console.log(`Bet size is ${web3.utils.fromWei(betSize, 'ether')} ether`);
         this.setState({
           betSize
         });
         web3.eth.getAccounts()
           .then((accounts) => {
-            console.log(`Player 1 is ${accounts[0]}.`);
-            console.log(`Player 2 is ${accounts[1]}.`);
+            console.log(`Player 1 is ${accounts[0]}`);
+            console.log(`Player 2 is ${accounts[1]}`);
             this.setState({
               player1: accounts[0],
               player2: accounts[1]
@@ -79,7 +79,7 @@ class App extends Component {
   }
 
   handleGameCreated(event) {
-    console.log(`Player 1 created new game with id ${event.returnValues.gameId}.`);
+    console.log(`Player 1 created new game with id ${event.returnValues.gameId}`);
     this.setState({
       gameId: event.returnValues.gameId
     });
@@ -91,14 +91,14 @@ class App extends Component {
   }
 
   handlePlayerJoined(event) {
-    console.log(`Player 2 has joined game.`);
+    console.log(`Player 2 has joined game`);
   }
 
   handleNextPlayer(event) {
     getBoard(this.state.gameId)
       .call()
       .then((board) => {
-        console.log(`Active player is ${event.returnValues.player}.`);
+        console.log(`Active player is ${event.returnValues.player}`);
         this.setState({
           activePlayer: event.returnValues.player,
           board
@@ -130,7 +130,7 @@ class App extends Component {
   }
 
   handlePayoutSuccess(event) {
-    console.log(`Transferred ${event.returnValues.amountInWei} wei to ${event.returnValues.recipient}.`);
+    console.log(`Transferred ${web3.utils.fromWei(event.returnValues.amountInWei, 'ether')} ether to ${event.returnValues.recipient}`);
   }
 
   render() {
