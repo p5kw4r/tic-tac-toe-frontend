dc0b57b21032d89011935b00e0bb90c7839537fd
Patrick Widmer <padi.widmer@gmail.com>
2018-03-25 01:24:06 +0100

    Clean up

--- src/Cell.js
+++ src/Cell.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
+const resolveSymbol = (address, player1, player2) => {
+  if (address === player1) {
+    return 'X';
+  } else if (address === player2) {
+    return 'O';
+  } else {
+    return '';
+  }
+};
+
 const Cell = ({ address, player1, player2, noAddress, onPlaceMarker }) => (
   <div
     className={`Cell ${address === noAddress ? 'valid' : 'invalid'}`}
     onClick={() => onPlaceMarker()}
   >
-    {
-      address === player1 ? 'X' : address === player2 ? 'O' : ''
-    }
+    {resolveSymbol(address, player1, player2)}
   </div>
 );
 
