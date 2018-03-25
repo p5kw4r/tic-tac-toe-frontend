772c267bcf0224ced04d8d0002a84a398c0f1598
Patrick Widmer <padi.widmer@gmail.com>
2018-03-25 21:55:44 +0200

    Check for existing Web3 Provider first

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Web3 from 'web3';
 import Board from './Board';
 import './App.css';
 
-const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'));
+const web3 = new Web3(Web3.givenProvider || new Web3.providers.WebsocketProvider('ws://localhost:8545'));
 const abi = [{"constant":true,"inputs":[],"name":"BET_SIZE","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"gameId","type":"uint256"}],"name":"GameCreated","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"player","type":"address"}],"name":"PlayerJoined","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"player","type":"address"}],"name":"NextPlayer","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"winner","type":"address"}],"name":"GameOverWithWin","type":"event"},{"anonymous":false,"inputs":[],"name":"GameOverWithDraw","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"recipient","type":"address"},{"indexed":false,"name":"amountInWei","type":"uint256"}],"name":"PayoutSuccess","type":"event"},{"constant":false,"inputs":[],"name":"createGame","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"gameId","type":"uint256"}],"name":"joinGame","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"gameId","type":"uint256"}],"name":"getBoard","outputs":[{"name":"","type":"address[3][3]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"gameId","type":"uint256"},{"name":"row","type":"uint8"},{"name":"column","type":"uint8"}],"name":"placeMark","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}];
 const address = '0x9adbf80e55ed9cf4f5dc76e338665f91bbfa02c3';
 
@@ -42,9 +42,6 @@ class App extends Component {
       case 'GameCreated':
         this.handleGameCreated(event);
         break;
-      // case 'PlayerJoined':
-      //   this.handlePlayerJoined();
-      //   break;
       case 'NextPlayer':
         this.handleNextPlayer(event);
         break;
@@ -55,9 +52,6 @@ class App extends Component {
       case 'GameOverWithDraw':
         this.handleGameOver('Game is over, ended with draw');
         break;
-      // case 'PayoutSuccess':
-      //   this.handlePayoutSuccess(event);
-      //   break;
       default:
         break;
     }
@@ -70,16 +64,12 @@ class App extends Component {
   async getBetSizeAsync() {
     const betSize = await BET_SIZE()
       .call();
-    // const betSizeInEther = web3.utils.fromWei(betSize, 'ether');
     this.setState({ betSize });
-    // console.log(`Bet size is ${betSizeInEther} ether`);
   }
 
   async getAccountsAsync() {
     const accounts = await web3.eth.getAccounts();
     this.setState({ player1: accounts[0], player2: accounts[1] });
-    // console.log(`Player 1 is ${accounts[0]}`);
-    // console.log(`Player 2 is ${accounts[1]}`);
   }
 
   async updateBoardAsync() {
@@ -98,7 +88,6 @@ class App extends Component {
   handleGameCreated({ returnValues: { gameId } }) {
     this.setState({ gameId });
     this.handleJoinGame();
-    // console.log(`Player 1 created new game with id ${gameId}`);
   }
 
   handleJoinGame() {
@@ -107,14 +96,9 @@ class App extends Component {
       .send({ from: player2, value: betSize });
   }
 
-  // handlePlayerJoined() {
-  //   console.log(`Player 2 has joined game`);
-  // }
-
   handleNextPlayer({ returnValues: { player } }) {
     this.setState({ activePlayer: player });
     this.updateBoardAsync();
-    // console.log(`Active player is ${player}`);
   }
 
   handlePlaceMark(column, row) {
@@ -127,16 +111,9 @@ class App extends Component {
 
   async handleGameOver(message) {
     await this.updateBoardAsync();
-    // console.log(message);
     alert(message);
     this.handleCreateGame();
   }
-
-  // handlePayoutSuccess(event) {
-  //   const { recipient, amountInWei } = event.returnValues;
-  //   const amountInEther = web3.utils.fromWei(amountInWei, 'ether');
-  //   console.log(`Transferred ${amountInEther} ether to ${recipient}`);
-  // }
 
   render() {
     const { board, player1, player2 } = this.state;
