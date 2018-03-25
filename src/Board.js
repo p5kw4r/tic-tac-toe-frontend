a26c55f862e8100267b0554916ac31254973160e
Patrick Widmer <padi.widmer@gmail.com>
2018-03-25 20:42:51 +0200

    Clean up

--- src/Board.js
+++ src/Board.js
@@ -3,20 +3,18 @@ import Cell from './Cell';
 
 const Board = ({ board, player1, player2, noAddress, onPlaceMarker }) => (
   <div className="Board">
-    {
-      board.map((column, i) => (
-        column.map((address, j) => (
-          <Cell
-            key={j}
-            address={address}
-            player1={player1}
-            player2={player2}
-            noAddress={noAddress}
-            onPlaceMarker={() => onPlaceMarker(i, j)}
-          />
-        ))
+    {board.map((column, i) => (
+      column.map((address, j) => (
+        <Cell
+          key={j}
+          address={address}
+          player1={player1}
+          player2={player2}
+          noAddress={noAddress}
+          onPlaceMarker={() => onPlaceMarker(i, j)}
+        />
       ))
-    }
+    ))}
   </div>
 );
 
