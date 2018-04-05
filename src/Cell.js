157daf40f60e14d54cc9330e366bdeb6411655d3
Patrick Widmer <padi.widmer@gmail.com>
2018-04-05 19:30:45 +0200

    Clean up

--- src/Cell.js
+++ src/Cell.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Cell = ({ address, players, noAddress: NO_ADDRESS, onPlaceMark }) => {
-  const status = address === NO_ADDRESS ? 'valid' : 'invalid';
+  const classes = `Cell noselect ${address === NO_ADDRESS ? 'valid' : 'invalid'}`;
   return (
-    <div className={`Cell noselect ${status}`} onClick={() => onPlaceMark()}>
+    <div className={classes} onClick={() => onPlaceMark()}>
       {address === players[0] ? 'X' : address === players[1] ? 'O' : ''}
     </div>
   );
