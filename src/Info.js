75bd90d7813af92165af9dbee79fb996f2e1439d
Patrick Widmer <padi.widmer@gmail.com>
2018-04-05 11:34:53 +0200

    Display loading message during initial setup

--- src/Info.js
+++ src/Info.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Info.css';
 
-const Info = ({ betSize, players, activePlayer, balances, noAddress, onFromWei }) => (
-  activePlayer !== noAddress && (
+const Info = ({ betSize, players, activePlayer, balances, noAddress, onFromWei }) => {
+  if (activePlayer === noAddress) {
+    return <div className="Info">Loading, please wait...</div>;
+  }
+
+  return (
     <div className="Info">
       <div>{`Bet Size: ${onFromWei(betSize)} ether`}</div>
       <div>{`Player 1: ${players[0]} - Balance: ${onFromWei(balances[0])} ether`}</div>
       <div>{`Player 2: ${players[1]} - Balance: ${onFromWei(balances[1])} ether`}</div>
       <div>{`Active Player: ${activePlayer === players[0] ? 'Player 1' : 'Player 2'}`}</div>
     </div>
-  )
-);
+  );
+};
 
 export default Info;
