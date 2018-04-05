c1a94881bc48fbff163e74e7ba4354d7c33420d3
Patrick Widmer <padi.widmer@gmail.com>
2018-04-05 11:26:37 +0200

    Clean up

--- src/Info.js
+++ src/Info.js
@@ -5,8 +5,8 @@ const Info = ({ betSize, players, activePlayer, balances, noAddress, onFromWei }
   activePlayer !== noAddress && (
     <div className="Info">
       <div>{`Bet Size: ${onFromWei(betSize)} ether`}</div>
-      <div>{`Player 1: ${players[0]} - ${onFromWei(balances[0])} ether`}</div>
-      <div>{`Player 2: ${players[1]} - ${onFromWei(balances[1])} ether`}</div>
+      <div>{`Player 1: ${players[0]} - Balance: ${onFromWei(balances[0])} ether`}</div>
+      <div>{`Player 2: ${players[1]} - Balance: ${onFromWei(balances[1])} ether`}</div>
       <div>{`Active Player: ${activePlayer === players[0] ? 'Player 1' : 'Player 2'}`}</div>
     </div>
   )
