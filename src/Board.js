490dcdafc2c66ed84bc2d05f6da726e972dacef9
Patrick Widmer <padi.widmer@gmail.com>
2018-04-07 09:41:03 +0200

    Remove loading screen

--- src/Board.js
+++ src/Board.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import Cell from './Cell';
-import logo from './logo.svg';
 
-const Board = ({ game: { board, activePlayer }, accounts, noAddress: NO_ADDRESS, onPlaceMark }) => {
-  if (activePlayer === NO_ADDRESS) {
-    return <img src={logo} alt="logo" width={800} />;
-  }
-
-  return (
-    <div className="Board">
-      {board.map((column, i) => (
-        column.map((address, j) => (
-          <Cell
-            key={j}
-            address={address}
-            accounts={accounts}
-            noAddress={NO_ADDRESS}
-            onPlaceMark={() => onPlaceMark(i, j)}
-          />
-        ))
-      ))}
-    </div>
-  );
-};
+const Board = ({ game: { board, activePlayer }, accounts, noAddress: NO_ADDRESS, onPlaceMark }) => (
+  <div className="Board">
+    {board.map((column, i) => (
+      column.map((address, j) => (
+        <Cell
+          key={j}
+          address={address}
+          accounts={accounts}
+          noAddress={NO_ADDRESS}
+          onPlaceMark={() => onPlaceMark(i, j)}
+        />
+      ))
+    ))}
+  </div>
+);
 
 export default Board;
