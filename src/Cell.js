d56c5161cd90d4bf4cfb2676f589cc6ae3fbfe54
Patrick Widmer <padi.widmer@gmail.com>
2018-04-08 21:13:48 +0200

    Clean up

--- src/Cell.js
+++ src/Cell.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 const Cell = ({ address, accounts, noAddress: NO_ADDRESS, onPlaceMark }) => {
+  let state, mark;
+  if (address === NO_ADDRESS) {
+    state = 'valid';
+    mark = ''
+  } else {
+    state = 'invalid';
+    if (address === accounts[0]) {
+      mark = 'X';
+    } else {
+      mark = 'O';
+    }
+  }
   return (
-    <div
-      className={`Cell no-select ${address === NO_ADDRESS ? 'valid' : 'invalid'}`}
-      onClick={() => onPlaceMark()}
-    >
-      {address === accounts[0] ? 'X' : address === accounts[1] ? 'O' : ''}
+    <div className={`Cell no-select ${state}`} onClick={() => onPlaceMark()}>
+      {mark}
     </div>
   );
 };
