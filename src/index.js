6804db3035629e8a1de6712f57b5c4d8dcb10d21
Patrick Widmer <padi.widmer@gmail.com>
2018-04-08 20:55:26 +0200

    Clean up

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Router>
@@ -12,4 +11,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
-registerServiceWorker();
