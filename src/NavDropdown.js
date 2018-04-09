0410933c10df2d49ac0b7c2d2651a93449888219
Patrick Widmer <padi.widmer@gmail.com>
2018-04-09 15:45:14 +0200

    Clean up

--- src/NavDropdown.js
+++ src/NavDropdown.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import {
   DropdownItem,
@@ -7,35 +7,28 @@ import {
   UncontrolledDropdown
 } from 'reactstrap';
 
-class NavDropdown extends Component {
-  navigateTo({ currentTarget: { value: gameId } }) {
-    this.props.history.push(`/${gameId}`);
-  }
-
-  render() {
-    const { games, match: { path } } = this.props;
-    return (
-      <UncontrolledDropdown nav inNavbar>
-        <DropdownToggle className="no-select" tag="span" nav caret>
-          Select Game
-        </DropdownToggle>
-        <DropdownMenu>
-          {Object.keys(games).map((gameId) => (
-            games[gameId].active && (
-              <DropdownItem
-                key={gameId}
-                value={gameId}
-                active={gameId === path.replace('/', '')}
-                onClick={(e) => this.navigateTo(e)}
-              >
-                {`Game ${gameId}`}
-              </DropdownItem>
-            )
-          ))}
-        </DropdownMenu>
-      </UncontrolledDropdown>
-    );
-  }
-}
+const NavDropdown = ({ games, match: { path }, history }) => (
+  <UncontrolledDropdown nav inNavbar>
+    <DropdownToggle className="no-select" tag="span" nav caret>
+      Select Game
+    </DropdownToggle>
+    <DropdownMenu>
+      {Object.keys(games).map((gameId) => (
+        games[gameId].active && (
+          <DropdownItem
+            key={gameId}
+            value={gameId}
+            active={gameId === path.replace('/', '')}
+            onClick={({ currentTarget: { value: gameId } }) => (
+              history.push(`/${gameId}`)
+            )}
+          >
+            {`Game ${gameId}`}
+          </DropdownItem>
+        )
+      ))}
+    </DropdownMenu>
+  </UncontrolledDropdown>
+);
 
 export default withRouter(NavDropdown);
