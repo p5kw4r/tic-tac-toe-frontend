52a9743cc69e656b33074205ed6d3029d085dc51
Patrick Widmer <padi.widmer@gmail.com>
2018-04-25 11:13:45 +0200

    Refactor dropdown in nav

--- src/NavDropdown.js
+++ src/NavDropdown.js
@@ -1,34 +1,51 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import {
+  Dropdown,
   DropdownItem,
   DropdownMenu,
-  DropdownToggle,
-  UncontrolledDropdown
+  DropdownToggle
 } from 'reactstrap';
 
-const NavDropdown = ({ games, match: { url }, history }) => (
-  <UncontrolledDropdown nav inNavbar>
-    <DropdownToggle className="no-select" tag="span" nav caret>
-      Select Game
-    </DropdownToggle>
-    <DropdownMenu>
-      {Object.keys(games).map((gameId) => (
-        games[gameId].active && (
-          <DropdownItem
-            key={gameId}
-            value={gameId}
-            active={gameId === url.replace('/game/', '')}
-            onClick={({ currentTarget: { value: gameId } }) => (
-              history.push(`/game/${gameId}`)
-            )}
-          >
-            {`Game ${gameId}`}
-          </DropdownItem>
-        )
-      ))}
-    </DropdownMenu>
-  </UncontrolledDropdown>
-);
+class NavDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false };
+  }
+
+  toggle() {
+    this.setState(({ isOpen }) => ({
+      isOpen: !isOpen
+    }));
+  }
+
+  render() {
+    const { games, match: { url }, history } = this.props;
+    const { isOpen } = this.state;
+    return (
+      <Dropdown nav inNavbar isOpen={isOpen} toggle={() => this.toggle()}>
+        <DropdownToggle className="no-select" tag="span" nav caret>
+          Select Game
+        </DropdownToggle>
+        <DropdownMenu>
+          {Object.keys(games).map((gameId) => (
+            games[gameId].active && (
+              <DropdownItem
+                key={gameId}
+                value={gameId}
+                active={gameId === url.replace('/game/', '')}
+                onClick={({ currentTarget: { value: gameId } }) => (
+                  history.push(`/game/${gameId}`)
+                )}
+              >
+                {`Game ${gameId}`}
+              </DropdownItem>
+            )
+          ))}
+        </DropdownMenu>
+      </Dropdown>
+    );
+  }
+}
 
 export default withRouter(NavDropdown);
