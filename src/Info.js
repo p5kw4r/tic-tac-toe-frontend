f004ade3fcdcfcf7c29e1d43705ac378f6585e23
Patrick Widmer <padi.widmer@gmail.com>
2018-04-25 11:59:28 +0200

    Clean up Refactor Info component

--- src/Info.js
+++ src/Info.js
@@ -12,17 +12,15 @@ const Info = ({ activePlayer, accounts, balances }) => (
       </tr>
       </thead>
       <tbody>
-      {accounts.map((account, i) => (
-        i < 2 && (
-          <tr
-            key={account}
-            className={account === activePlayer ? 'active' : ''}
-          >
-            <th scope="row">{i + 1}</th>
-            <td>{i === 0 ? 'Player X' : 'Player O'}</td>
-            <td>{balances[i]}</td>
-          </tr>
-        )
+      {balances.map((balance, i) => (
+        <tr
+          key={accounts[i]}
+          className={accounts[i] === activePlayer ? 'active' : ''}
+        >
+          <th scope="row">{i + 1}</th>
+          <td>{i === 0 ? 'Player X' : 'Player O'}</td>
+          <td>{balance}</td>
+        </tr>
       ))}
       </tbody>
     </Table>
